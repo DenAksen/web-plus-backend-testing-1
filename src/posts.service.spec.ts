6a34b27b3eac4cb33227aa9c727d587d33bae6e7
Denis Aksenov <DAksenovE@yandex.ru>
2026-05-13 19:45:48 +0300

    add: test

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,31 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const newPostText = 'Mocked post';
+    
+    const createdPost = postsService.create({ text: newPostText });
+    
+    // Assert
+    expect(createdPost).toBeDefined();
+    expect(createdPost.id).toBeDefined();
+    expect(createdPost.id).toBe('2');
+    expect(createdPost.text).toBe(newPostText);
+    expect(createdPost.date).toBeDefined();
+    expect(typeof createdPost.date).toBe('string');
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    // Arrange
+    const createdPost = postsService.create({ text: 'Post to find' });
+    const expectedPostId = createdPost.id;
+    
+    // Act
+    const foundPost = postsService.find(expectedPostId);
+    
+    // Assert
+    expect(foundPost).toBeDefined();
+    expect(foundPost?.id).toBe(expectedPostId);
+    expect(foundPost?.text).toBe('Post to find');
+    expect(foundPost?.date).toBeDefined();
   });
 });
